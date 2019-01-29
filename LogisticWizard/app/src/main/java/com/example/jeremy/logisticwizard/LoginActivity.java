29c27542af211013bfcfd01c0b1b60da5660f383
Zhufeng Chen <zche33@ucsc.edu>
2019-01-28 18:22:04 -0800

    login page without forget pwd

--- LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/LoginActivity.java
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/LoginActivity.java
@@ -2,12 +2,25 @@ package com.example.jeremy.logisticwizard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.support.v7.app.ActionBar;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(8);
         setContentView(R.layout.activity_login);
+        View decorView = getWindow().getDecorView();
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
     }
 }
