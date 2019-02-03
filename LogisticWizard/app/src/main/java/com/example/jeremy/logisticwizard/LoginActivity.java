dcc2832cba15aa1c54547d9a57ad8f446622c54b
Jinghao Hao <jeremyhao1125@outlook.com>
2019-02-02 19:25:12 -0800

    new

--- LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/LoginActivity.java
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/LoginActivity.java
@@ -1,21 +1,43 @@
 package com.example.jeremy.logisticwizard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
-import com.google.firebase.database.DatabaseReference;
+import android.view.View;
+import android.support.v7.app.ActionBar;
 
 public class LoginActivity extends AppCompatActivity {
-    private DatabaseReference mDatabase;
-
-
+    private Button SignupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(8);
         setContentView(R.layout.activity_login);
 
+        //invoking Button
+        SignupButton=(Button)findViewById(R.id.SignUpBut);
+        SignupButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                if (v==SignupButton){
+                    Intent intent = new Intent(v.getContext(), RegisterActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
 
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
     }
 }
--- LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/RegisterActivity.java
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/RegisterActivity.java
@@ -2,22 +2,41 @@ package com.example.jeremy.logisticwizard;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.auth.FirebaseAuth;
 
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText getEmail;
-
+    private Button SubmitBtn;
     private DatabaseReference mDatabase;
+    //private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        //mAuth = FirebaseAuth.getInstance();
         getEmail = (EditText) findViewById(R.id.enterEmail);
+        SubmitBtn = (Button) findViewById(R.id.button2);
+        SubmitBtn.setOnClickListener((View.OnClickListener) this);
+    }
+
+    private void UserRegister(){
+        String UserEmail = getEmail.getText().toString().trim();
+        mDatabase.child("users").child("email").setValue(UserEmail);
+    }
+
+
+    public void onClick(View view) {
+        if(view == SubmitBtn){
+           UserRegister();
+        }
     }
 
 }
