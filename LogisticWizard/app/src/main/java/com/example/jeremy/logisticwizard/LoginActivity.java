ca22538a12fa0d899ae5ca0d8dbcece8dd4b1925
ZikeP <zpeng4@ucsc.edu>
2019-02-02 23:03:19 -0800

    can read data from database and can login by enter key

--- LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/LoginActivity.java
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/LoginActivity.java
@@ -1,16 +1,29 @@
 package com.example.jeremy.logisticwizard;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.view.View;
-
-import android.view.View;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
     private Button SignupButton;
+    private Button LoginButton;
+    private EditText user_name;
+    private EditText password;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,9 @@ public class LoginActivity extends AppCompatActivity {
 
         //invoking Button
         SignupButton=(Button)findViewById(R.id.SignUpBut);
+        LoginButton = (Button)findViewById(R.id.LoginBut);
+
+
         SignupButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
@@ -30,6 +46,65 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("username");
+        user_name = (EditText)findViewById(R.id.UserName);
+        password = (EditText) findViewById(R.id.Password);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user_name_s = user_name.getText().toString();
+                final String password_s = password.getText().toString();
+                mDatabase.child(user_name_s).child("password").addValueEventListener(new ValueEventListener(){
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        if(password_s.equals(dataSnapshot.getValue())){
+                            Toast.makeText(LoginActivity.this, "Login succeed", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Toast.makeText(LoginActivity.this, "No permission", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+
+        password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    //Toast.makeText(LoginActivity.this, "Press Enter", Toast.LENGTH_SHORT).show();
+                    String user_name_s = user_name.getText().toString();
+                    final String password_s = password.getText().toString();
+                    mDatabase.child(user_name_s).child("password").addValueEventListener(new ValueEventListener(){
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                if(password_s.equals(dataSnapshot.getValue())){
+                                    Toast.makeText(LoginActivity.this, "Login succeed", Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                                }
+
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            Toast.makeText(LoginActivity.this, "No permission", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                return false;
+            }
+        });
+
+
+
         View decorView = getWindow().getDecorView();
         // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
@@ -39,5 +114,10 @@ public class LoginActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
