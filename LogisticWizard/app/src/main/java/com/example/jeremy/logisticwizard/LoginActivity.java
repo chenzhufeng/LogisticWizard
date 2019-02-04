08149061d60bfbca60870d9c5732ddf959c59a02
chenzhufeng <zche33@ucsc.edu>
2019-02-04 00:06:40 -0800

    Fixed status bar

--- LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/LoginActivity.java
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/LoginActivity.java
@@ -29,11 +29,10 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-        View decorView = getWindow().getDecorView();
+        //View decorView = getWindow().getDecorView();
 // Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
+        //int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        //decorView.setSystemUiVisibility(uiOptions);
 // Remember that you should never show the action bar if the
 // status bar is hidden, so hide that too if necessary.
         ActionBar actionBar = getSupportActionBar();
