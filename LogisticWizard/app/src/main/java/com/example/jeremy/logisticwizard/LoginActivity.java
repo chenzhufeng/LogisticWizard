cf2b0953547ed99ded076d75d65b55c00abe4575
Jinghao Hao <jeremyhao1125@outlook.com>
2019-02-04 16:45:29 -0800

    login page works

--- LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/LoginActivity.java
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/LoginActivity.java
@@ -19,7 +19,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private Button SignupButton;
     private Button LoginButton;
     private EditText user_name;
@@ -35,46 +35,13 @@ public class LoginActivity extends AppCompatActivity {
 
         //invoking Button
         SignupButton=(Button)findViewById(R.id.SignUpBut);
+        SignupButton.setOnClickListener(this);
         LoginButton = (Button)findViewById(R.id.LoginBut);
-
-
-        SignupButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                if (v==SignupButton){
-                    Intent intent = new Intent(v.getContext(), RegisterActivity.class);
-                    startActivity(intent);
-                }
-            }
-        });
-
+        LoginButton.setOnClickListener(this);
 
         mDatabase = FirebaseDatabase.getInstance().getReference("user");
         user_name = (EditText)findViewById(R.id.UserName);
         password = (EditText) findViewById(R.id.Password);
-        LoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String user_name_s = user_name.getText().toString();
-                final String password_s = password.getText().toString();
-                mDatabase.child(user_name_s).child("password").addValueEventListener(new ValueEventListener(){
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-
-                        if(password_s.equals(dataSnapshot.getValue())){
-                            Toast.makeText(LoginActivity.this, "Login succeed", Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-                        }
-
-                    }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        Toast.makeText(LoginActivity.this, "No permission", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
 
 
         password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -118,18 +85,48 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+
     @Override
     protected void onStart() {
         super.onStart();
-        View decorView = getWindow().getDecorView();
-// Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-// Remember that you should never show the action bar if the
-// status bar is hidden, so hide that too if necessary.
+        //View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        //int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+        //decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == SignupButton) {
+            Intent intent = new Intent(view.getContext(), RegisterActivity.class);
+            startActivity(intent);
+
+        }
+        if (view == LoginButton) {
+            String user_name_s = user_name.getText().toString();
+            final String password_s = password.getText().toString();
+            mDatabase.child(user_name_s).child("password").addValueEventListener(new ValueEventListener(){
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    if(password_s.equals(dataSnapshot.getValue())){
+                        Toast.makeText(LoginActivity.this, "Login succeed", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Toast.makeText(LoginActivity.this, "No permission", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
--- LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/RegisterActivity.java
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/RegisterActivity.java
@@ -1,43 +1,19 @@
 package com.example.jeremy.logisticwizard;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
-import android.widget.EditText;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.auth.FirebaseAuth;
-
-
 public class RegisterActivity extends AppCompatActivity {
-    private EditText getEmail;
-    private Button SubmitBtn;
-    private DatabaseReference mDatabase;
-    //private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        //mAuth = FirebaseAuth.getInstance();
-        getEmail = (EditText) findViewById(R.id.enterEmail);
-        SubmitBtn = (Button) findViewById(R.id.button2);
-        SubmitBtn.setOnClickListener((View.OnClickListener) this);
-    }
-
-    private void UserRegister(){
-        String UserEmail = getEmail.getText().toString().trim();
-        mDatabase.child("users").child("email").setValue(UserEmail);
-    }
-
-
-    public void onClick(View view) {
-        if(view == SubmitBtn){
-           UserRegister();
-        }
         configureBackButton();
         configureSubButton();
     }
@@ -66,6 +42,4 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
-
 }
-
