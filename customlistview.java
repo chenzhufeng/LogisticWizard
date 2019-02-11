3d0fa5f326706aed9ae0df169f79cd4cb580ed69
connorjmonson <31049858+connorjmonson@users.noreply.github.com>
2019-02-10 18:04:24 -0800

    inventory page with all other necessities

---
+++ customlistview.java
@@ -0,0 +1,62 @@
+package com.example.connormonson.logisticwizard_inventory_page;
+
+import android.app.Activity;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class customlistview extends ArrayAdapter<String> {
+
+    private String[] inventorName;
+    private String[] inventorDescription;
+    private Activity context;
+
+    public customlistview(Activity context, String[] inventorName, String[] inventorDescription) {
+        super(context, R.layout.inventory_layout, inventorName);
+
+        this.context=context;
+        this.inventorName=inventorName;
+        this.inventorDescription=inventorDescription;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View r=convertView;
+            viewHolder viewHolder=null;
+            if (r==null){
+                LayoutInflater layoutInflater = context.getLayoutInflater();
+                r=layoutInflater.inflate(R.layout.inventory_layout,null,true);
+                viewHolder = new viewHolder(r);
+                r.setTag(viewHolder);
+            }
+            else{
+                viewHolder = (viewHolder) r.getTag();
+            }
+            viewHolder.tvw1.setText(inventorName[position]);
+            viewHolder.tvw2.setText(inventorDescription[position]);
+
+
+        return r;
+
+
+    }
+
+    class viewHolder{
+        TextView tvw1;
+        TextView tvw2;
+        viewHolder(View v){
+            tvw1 = (TextView) v.findViewById(R.id.sectionName);
+            tvw2 = (TextView) v.findViewById(R.id.sectionDescription);
+
+        }
+    }
+}
