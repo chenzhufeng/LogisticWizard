da556187df8cdad081f9a71d7e04937fb4e045a6
Zhufeng Chen <zche33@ucsc.edu>
2019-02-11 18:18:09 -0800

    Disable Full Screen Mode

--- LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/Login.java
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/Login.java
@@ -92,10 +92,10 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
 
 
-        View decorView = getWindow().getDecorView();
+        //View decorView = getWindow().getDecorView();
         // Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
+        //int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        //decorView.setSystemUiVisibility(uiOptions);
         // Remember that you should never show the action bar if the
         // status bar is hidden, so hide that too if necessary.
         //ActionBar actionBar = getSupportActionBar();
