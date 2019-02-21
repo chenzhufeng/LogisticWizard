febaedc500f08e8797aa6d371b482fef8fe3f7d3
Zhufeng Chen <zche33@ucsc.edu>
2019-02-20 19:45:49 -0800

    Machine display done

---
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/editMachine.java
@@ -0,0 +1,13 @@
+package com.example.jeremy.logisticwizard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editMachine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_machine);
+    }
+}
--- LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/machineDisp.java
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/machineDisp.java
@@ -1,22 +1,74 @@
 package com.example.jeremy.logisticwizard;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-public class machineDisp extends Activity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class machineDisp extends Activity implements View.OnClickListener{
+
+    private ListView lv;
+    private Button editButton;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.machinedisp_layout);
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
 
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-
-        //getWindow().setLayout((int) (width * .9), (int) (height * .8));
+        editButton = findViewById(R.id.editMachineButton);
+        editButton.setOnClickListener(this);
+        lv = findViewById(R.id.machineInfoList);
 
 
+
+        final ArrayList<String> listData = new ArrayList<String>();
+        LinkedHashMap<String, String> machineInfoHashMap = new LinkedHashMap<>();
+        machineInfoHashMap.put("Name", "Name data here");
+        machineInfoHashMap.put("Type", "Type data here");
+        machineInfoHashMap.put("Parts information", "Parts data here");
+        machineInfoHashMap.put("Price", "Price data here");
+        machineInfoHashMap.put("Location", "Location data here");
+        machineInfoHashMap.put("Maintenance Plan", "Maintenance data here");
+        machineInfoHashMap.put("Description", "Description data");
+
+
+        List<HashMap<String, String>> listItems = new ArrayList<>();
+        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.layoutfor_machineinfolist,
+                new String[]{"First Line", "Second Line"},
+                new int[]{R.id.item, R.id.data});
+
+        Iterator it = machineInfoHashMap.entrySet().iterator();
+        while (it.hasNext())
+        {
+            HashMap<String, String> resultsMap = new HashMap<>();
+            Map.Entry pair = (Map.Entry)it.next();
+            resultsMap.put("First Line", pair.getKey().toString());
+            resultsMap.put("Second Line", pair.getValue().toString());
+            listItems.add(resultsMap);
+        }
+
+        lv.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == editButton) {
+            Intent intent = new Intent(v.getContext(), editMachine.class);
+            startActivity(intent);
+        }
     }
 }
