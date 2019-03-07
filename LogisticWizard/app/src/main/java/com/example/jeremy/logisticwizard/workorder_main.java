8a606299a351168c481e2e71cc2025c2865e550e
Teijien <43975613+Teijien@users.noreply.github.com>
2019-03-06 22:19:52 -0800

    Last commit of day

--- LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/workorder_main.java
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/workorder_main.java
@@ -5,9 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+//import android.support.v7.widget.RecyclerView;
+//import android.support.v7.widget.LinearLayoutManager;
 
 public class workorder_main extends AppCompatActivity implements View.OnClickListener {
     private Button newOrder;
+    //private RecyclerView.LayoutManager linearLayoutManager;
+    //ArrayList workOrders = new ArrayList<>(Arrays.asList("Person 1, Person 2, Person 3"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,13 @@ public class workorder_main extends AppCompatActivity implements View.OnClickLis
 
         newOrder = findViewById(R.id.new_order);
         newOrder.setOnClickListener(this);
+
+        //RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        //recyclerView.setHasFixedSize(true);
+        //linearLayoutManager = new LinearLayoutManager(this);
+        //recyclerView.setLayoutManager(linearLayoutManager);
+        //mAdapter = new MyAdapter(myDataset);
+        //recyclerView.setAdapter(mAdapter);
     }
 
     @Override
