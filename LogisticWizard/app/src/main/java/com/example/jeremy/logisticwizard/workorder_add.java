acc391696f723b8453135d2ead82dee1050ec4f4
Jinghao Hao <jeremyhao1125@outlook.com>
2019-03-06 20:48:08 -0800

    upload image

--- LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/workorder_add.java
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/workorder_add.java
@@ -1,5 +1,9 @@
 package com.example.jeremy.logisticwizard;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,13 +19,25 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.EditText;
+import android.net.Uri;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class workorder_add extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     final static int galleryPic = 1;
 
     private TextInputEditText orderTitle;
     private TextInputEditText orderDescription;
-    private TextInputEditText orderImage;
     private Spinner orderPriority;
     private Spinner orderPlanSpinner;
     private TextInputEditText orderNote;
@@ -31,15 +47,19 @@ public class workorder_add extends AppCompatActivity implements View.OnClickList
     private Button submit;
     private ImageButton image;
 
+    private FirebaseStorage storage;
+    StorageReference storageReference;
+    private Uri filePath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.workorder_add);
-        orderTitle = findViewById(R.id.titleInput);
-        orderDescription = findViewById(R.id.textInputLayout);
+        orderTitle = findViewById(R.id.orderTitle);
+        orderDescription = findViewById(R.id.orderDescription);
         image = findViewById(R.id.imageButton);
-        orderNote = findViewById(R.id.textInputLayout2);
+        orderNote = findViewById(R.id.orderNote);
         orderDueDate = findViewById(R.id.dueDateInput);
         orderCost = findViewById(R.id.orderCost);
         submit = findViewById(R.id.submitButton);
@@ -65,6 +85,10 @@ public class workorder_add extends AppCompatActivity implements View.OnClickList
         orderPlanSpinner.setAdapter(adapter2);
         orderPlanSpinner.setOnItemSelectedListener(this);
 
+
+        storage=FirebaseStorage.getInstance("gs://logisticwizard-6d896.appspot.com/");
+        storageReference = storage.getReference();
+
     }
 
 
@@ -75,12 +99,9 @@ public class workorder_add extends AppCompatActivity implements View.OnClickList
             //Intent intent = new Intent(view.getContext(),order.class);
             //startActivity(intent);
         }
-//        if(view == image){
-//            Intent pictureIntent = new Intent();
-//            pictureIntent.setAction(Intent.ACTION_PICK);
-//            pictureIntent.setType("image/*");
-//            startActivityForResult(pictureIntent, galleryPic);
-//        }
+        if(view == image){
+            chooseImage();
+        }
     }
 
     @Override
@@ -91,6 +112,36 @@ public class workorder_add extends AppCompatActivity implements View.OnClickList
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    private void chooseImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_PICK);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 71);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 71 && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                float scale = this.getResources().getDisplayMetrics().density;
+                int width = (int)(350*scale+0.5f);
+                int height = (int)(200*scale+0.5f);
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                Picasso.with(this).load(filePath).resize(width, height).into(image);
+                //image.setImageBitmap(bitmap);
+
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
@@ -128,6 +179,42 @@ public class workorder_add extends AppCompatActivity implements View.OnClickList
             order_intent.putExtra("orderPriority", order_priority);
             order_intent.putExtra("maintainencePlan", maintainPlan);
             order_intent.putExtra("orderStatus", order_status);
+
+
+
+            if(filePath != null)
+            {
+                final ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.setTitle("Uploading...");
+                progressDialog.show();
+
+                StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
+                ref.putFile(filePath)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                progressDialog.dismiss();
+                                Toast.makeText(workorder_add.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                progressDialog.dismiss();
+                                Toast.makeText(workorder_add.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                        .getTotalByteCount());
+                                progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                            }
+                        });
+            }
+
+
             setResult(RESULT_OK, order_intent);
             finish();
         }
