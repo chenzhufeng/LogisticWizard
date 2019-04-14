e34f209ab740af7d7acb4cff8e7a8b21c268c00e
ZikeP <zpeng4@ucsc.edu>
2019-04-13 21:18:45 -0700

    Copy from Connor

---
+++ LogisticWizard/app/src/main/java/com/example/jeremy/logisticwizard/calendar_main_fragment.java
@@ -0,0 +1,19 @@
+package com.example.jeremy.logisticwizard;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jeremy.logisticwizard.R;
+
+public class calendar_main_fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.calendar_main_fragment, container, false);
+    }
+}
